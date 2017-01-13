1d014c9bd1776c73fcff4e63232f0130d1d91b20
Szabo, Peter (Agoda) <peter.szabo@agoda.com>
2017-01-13 19:53:14 +0700

    Fix brain-hurt

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -19,22 +19,28 @@ namespace downr.Controllers
         {
             // if no slug was provided show the last one
             if (string.IsNullOrEmpty(id))
+            {
                 return RedirectToAction("Index", "Posts", new
                 {
                     slug = _indexer.Metadata.ElementAt(0).Slug
                 });
+            }
 
             // if the slug was provided AND found, redirect to it
             if (_indexer.Metadata.Any(x => x.Slug == id))
+            {
                 return RedirectToAction("Index", "Posts", new
                 {
                     slug = _indexer.Metadata.ElementAt(0).Slug
                 });
+            }
             else // no match was found, show the last one
+            {
                 return RedirectToAction("Index", "Posts", new
                 {
                     slug = _indexer.Metadata.ElementAt(0).Slug
                 });
+            }
         }
     }
 }
